303aebbbb8824067e2ac290f3e5ac42634704e10
github username <github email address>
2020-01-30 17:28:23 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-    BRANDS.uniq!
+    # BRANDS << brand
+    # BRANDS.uniq!
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
